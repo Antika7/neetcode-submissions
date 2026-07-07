9366900165b69fa6815bd03f8b34ca3cf4c9b4c3
Antika Burman <87443018+Antika7@users.noreply.github.com>
2026-07-07 15:22:53 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int[] result = new int[2];
+
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+
+        ArrayList<Integer> numList = new ArrayList<>();
+        ArrayList<Integer> required = new ArrayList<>();
+
+        for(int i=0; i<numbers.length; i++) {
+            numMap.put(target-numbers[i], i);
+        }
+
+        for(int i=0; i<numbers.length; i++) {
+            numList.add(numbers[i]);
+        }
+
+        for(int i=0; i<numbers.length; i++) {
+            required.add(target-numbers[i]);
+        }
+
+        int i = 0;
+
+        for(int num : numList) {
+
+            System.out.println("\nFirst number is " + num);
+            System.out.println("Required number is " + required.get(i));
+
+            System.out.println("\nIs it there in list? " + numList.contains(required.get(i)));
+            if(numList.contains(required.get(i))) {
+                result[0] = i+1;
+                System.out.println("result[0] : " + (i+1));
+
+                System.out.println("\nPosition of " + required.get(i));
+                result[1] = numList.indexOf(required.get(i)) + 1;
+                System.out.println("result[1] : " + result[1]);
+                break;
+            }
+            i++;
+        }
+
+        return result;
+        
+    }
+}
